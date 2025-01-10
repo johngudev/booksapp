0a8fe3ce3f4af514d330b985656aebce076b169b
John <johngucs@gmail.com>
2025-01-09 21:56:12 -0600

    center books page

--- resources/js/books/components/BooksPage.tsx
+++ resources/js/books/components/BooksPage.tsx
@@ -39,7 +39,7 @@ const BooksPage = () => {
                 placeholder="Search for a book"
                 value={searchValue}
             />
-            <div className="py-12 max-w-6xl mx-auto px-6 lg:px-8 flex md:flex-row flex-column flex-col gap-3 flex-wrap">
+            <div className="py-12 max-w-6xl mx-auto px-6 lg:px-8 flex md:flex-row flex-column flex-col gap-3 flex-wrap justify-center">
                 {filteredBooks.map((book) => {
                     const { id, image_url: imageUrl, author, title } = book;
                     return (
