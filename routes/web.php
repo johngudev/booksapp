447622d2727afa18a739c53ebcfe8e0b60576570
John <johngucs@gmail.com>
2025-01-12 10:59:15 -0600

    rename more routes

--- routes/web.php
+++ routes/web.php
@@ -54,8 +54,8 @@ Route::middleware('auth')->post('/api/session/books/{book}',[BookController::cla
 Route::middleware('auth')->delete('/api/session/books/{book}',[BookController::class, 'unlinkFromUser']);
 
 Route::middleware('auth')->get('/api/meetings/join/{meeting}',[MeetingController::class, 'linkedUsers']);
-Route::middleware('auth')->post('/api/meetings/join/{meeting}',[MeetingController::class, 'linkToUser']);
-Route::middleware('auth')->delete('/api/meetings/join/{meeting}',[MeetingController::class, 'unlinkFromUser']);
+Route::middleware('auth')->post('/api/session/meetings/join/{meeting}',[MeetingController::class, 'linkToUser']);
+Route::middleware('auth')->delete('/api/session/meetings/join/{meeting}',[MeetingController::class, 'unlinkFromUser']);
 
 Route::middleware('auth')->post('/api/session/meetings/host',[MeetingController::class, 'storeFromSession'])->name('session.meetings.host.store');
 
