b8aeecf604eb6c9573ee59079154a96229f8d0e8
John <johngucs@gmail.com>
2025-01-11 22:26:09 -0600

    data-api-books

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-<div id="react-root" data-books="{{$books}}"></div>
+<div id="react-root" data-api-books="{{ route('books.list') }}" data-books="{{$books}}"></div>
 
     <!-- <form class="max-w-md mx-auto mt-12">   
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
