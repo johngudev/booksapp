6312234f3048705648959b187f8631a3b9e18b68
John <johngucs@gmail.com>
2025-01-22 18:38:18 -0600

    meetings named routes in html data

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-<div id="meetings-root" data-api-meetings-index="{{ route('meetings.index') }}" data-api-session-books="{{ route('session.meetings.index') }}"></div>
+<div id="meetings-root" data-api-meetings-index="{{ route('meetings.index') }}" data-api-session-meetings-host="{{ route('session.meetings.host') }}" data-api-session-meetings-join="{{ route('session.meetings.join') }}"></div>
 
 </x-app-layout>
 
