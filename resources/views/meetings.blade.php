78327d55209b6439bb36082c5fef924c8f3eede2
John <johngucs@gmail.com>
2025-01-22 21:21:35 -0600

    added bottom padding to meetings

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-<div id="meetings-root" data-api-meetings-index="{{ route('meetings.index') }}" data-api-session-meetings-host="{{ route('session.meetings.host') }}" data-api-session-meetings-join="{{ route('session.meetings.join') }}"></div>
+<div class="pb-12" id="meetings-root" data-api-meetings-index="{{ route('meetings.index') }}" data-api-session-meetings-host="{{ route('session.meetings.host') }}" data-api-session-meetings-join="{{ route('session.meetings.join') }}"></div>
 
 </x-app-layout>
 
