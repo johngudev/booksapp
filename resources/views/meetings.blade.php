9010a5f262da99037ca2efc62a8baf8e9447c383
John <johngucs@gmail.com>
2025-01-22 18:35:16 -0600

    added meetings info to page

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-<div id="meetings-root"></div>
+<div id="meetings-root" data-api-meetings-index="{{ route('meetings.index') }}" data-api-session-books="{{ route('session.meetings.index') }}"></div>
 
 </x-app-layout>
 
