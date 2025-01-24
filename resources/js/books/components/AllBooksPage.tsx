dd48aff437469ef60e2a7198524d32a6b6894a51
John <johngucs@gmail.com>
2025-01-23 20:42:16 -0600

    tweak card size for mobile

--- resources/js/books/components/AllBooksPage.tsx
+++ resources/js/books/components/AllBooksPage.tsx
@@ -92,7 +92,7 @@ const AllBooksPage = () => {
                     />
                 }
             />
-            <div className="mt-12 flex md:flex-row flex-column flex-col gap-3 flex-wrap">
+            <div className="mt-12 flex sm:flex-row flex-row  gap-3 flex-wrap">
                 <LoadingWrapper loading={loading}>
                     {books ? (
                         <>
